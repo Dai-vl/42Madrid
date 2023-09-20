97f92a770c37153187e078eb43006fe5897ebe78
Daniela <91054958+Dai-vl@users.noreply.github.com>
2023-09-20 18:38:17 +0200

    Arreglado el makefile

--- 42Cursus/Libft/ft_lstmap_bonus.c
+++ 42Cursus/Libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dvidal-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:17:56 by dvidal-l          #+#    #+#             */
-/*   Updated: 2023/09/14 23:16:35 by dvidal-l         ###   ########.fr       */
+/*   Updated: 2023/09/20 17:17:28 by dvidal-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*ret;
+	void	*cont;
 
 	if (!lst || !f || !del)
 		return (0);
 	ret = 0;
 	while (lst)
 	{
-		new = ft_lstnew((*f)(lst->content));
+		cont = (*f)(lst->content);
+		new = ft_lstnew(cont);
 		if (new == 0)
 		{
+			(*del)(cont);
 			ft_lstclear(&ret, del);
 			return (0);
 		}
