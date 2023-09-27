7c6297298762ab457b3d9c459c7debc43fae8d39
Daniela <91054958+Dai-vl@users.noreply.github.com>
2023-09-27 19:09:42 +0200

    memset calloc

--- 42Cursus/Libft/ft_calloc.c
+++ 42Cursus/Libft/ft_calloc.c
@@ -6,24 +6,11 @@
 /*   By: dvidal-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 19:51:59 by dvidal-l          #+#    #+#             */
-/*   Updated: 2023/09/27 19:07:49 by dvidal-l         ###   ########.fr       */
+/*   Updated: 2023/09/27 19:08:56 by dvidal-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	*ft_memset(void *b, int c, size_t len)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < len)
-	{
-		((unsigned char *)b)[i] = (unsigned char) c;
-		++i;
-	}
-	return (b);
-}
 
 void	*ft_calloc(size_t count, size_t size)
 {
